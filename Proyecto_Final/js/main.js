f9970f9b91404a299f3271de9cd387b2bd9ba1dd
Barbieri <barbieriaugusto62@gmail.com>
2026-03-19 08:32:13 -0300

    feat: Initialize final project structure with `index.html`, `style.css`, and `main.js` for the final project.

---
+++ Proyecto_Final/js/main.js
@@ -0,0 +1,8 @@
+// Archivo principal de Javascript para el Proyecto Final
+console.log("¡El Proyecto Final está conectado a JS!");
+
+// Ejemplo: Si quisieras reproducir un audio desde JS referenciando la carpeta Recursos
+/*
+const miAudio = new Audio("../Recursos/audio/sonido.mp3");
+miAudio.play();
+*/
